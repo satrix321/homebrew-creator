b18e28e0322a0a44e4de346c8f3ad23c8bfcadaf
unknown <k.szot@atechno.pl>
2018-03-30 16:08:48 +0200

    vue start

---
+++ phb-pdf-generator/src/components/Editor.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="editor">
+    <p>Hello, I'm an editor!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Editor'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ phb-pdf-generator/src/components/Main.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main">
+    <Split ref="split1" :gutterSize="10" @onDragEnd="onDragEnd" style="height: 100vh;">
+      <SplitArea :size="50">
+          Sedd ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eodolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+      </SplitArea>
+      <SplitArea :size="50">
+          Sedd ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eodolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+      </SplitArea>
+    </Split>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
