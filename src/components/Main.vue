6b850c9bcfa61c63e6329f44357018e310949a35
Krzysztof Szot <krzysztof.szot@cognifide.com>
2018-05-16 15:59:53 +0200

    style restore

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -27,6 +27,22 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
-$blueprint-grid-columns: 3;
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.editorArea {
+  overflow-y: hidden;
+}
 </style>
