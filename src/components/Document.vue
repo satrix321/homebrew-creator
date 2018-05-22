ec7de509caebf65b49ac5b9b612b0c97dd43df9a
Krzysztof Szot <krzysztof.szot@cognifide.com>
2018-05-22 21:16:59 +0200

    x scroll fix while docs is wider than panel

--- src/components/Document.vue
+++ src/components/Document.vue
@@ -217,7 +217,7 @@ export default {
 
     .page {
       display: block;
-      margin: 0 0 0.5cm 0;
+      margin: 0 auto 0.5cm auto;
       box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
       background-color: white;
       position: relative;
