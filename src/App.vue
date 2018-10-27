85ab0bc0e256b74058bc4c0d9ea2ff4ab357bee8
unknown <satrix321@gmail.com>
2018-10-27 22:08:37 +0200

    Refactor - removed vue-router - it wasn't used - merged App & Main component - removed some scss files, moved styles so it's easier to maintain

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="main">
+      <split :gutterSize="10" style="height: 100vh;" @onDrag="splitDrag">
+        <split-area class="split-editor" :size="50">
+          <editor-item/>
+        </split-area>
+        <split-area class="split-document" :size="50">
+          <document-item :eventBus="documentEventBus"/>
+        </split-area>
+      </split>
+    </div>
   </div>
 </template>
 
 <script>
+import EditorItem from '@/components/Editor';
+import DocumentItem from '@/components/Document';
+import Vue from 'vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    EditorItem,
+    DocumentItem
+  },
+  data: function () {
+    return {
+      documentEventBus: new Vue()
+    };
+  },
+  created: function () {
+    window.addEventListener('resize', this.windowResized);
+    window.onbeforeprint = this.onBeforePrint;
+  },
+  methods: {
+    splitDrag: function () {
+      this.documentEventBus.$emit('resize');
+    },
+    windowResized: function () {
+      this.documentEventBus.$emit('resize');
+    },
+    onBeforePrint: function () {
+      this.documentEventBus.$emit('onBeforePrint');
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-#app {
-  @include font-smoothing;
+@import "@/assets/scss/fonts.scss";
+@import "@/assets/scss/print.scss";
+body {
+  margin: 0px;
+  overflow-x: hidden;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import router from './router';
 import store from './store';
 import VueSplit from 'vue-split-panel';
 
@@ -15,7 +14,6 @@ Vue.use(VueSplit);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount('#app');
